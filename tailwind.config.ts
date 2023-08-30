280f4d23973fdc5ca6051884d964464fd0d81e42
codersuresh <sd807141@gmail.com>
2023-08-30 07:45:30 +0545

    use - instead of _ for color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,13 +9,13 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        'marine_blue': 'var(--marine-blue)',
-        'purplish_blue': 'var(--purplish-blue)',
-        'light_blue': 'var(--light-blue)',
-        'pastel_blue': 'var(--pastel-blue)',
-        'strawberry_red': 'var(--strawberry-red)',
-        'cool_grey': 'var(--cool-grey)',
-        'light_grey': 'var(--light-grey)',
+        'marine-blue': 'var(--marine-blue)',
+        'purplish-blue': 'var(--purplish-blue)',
+        'light-blue': 'var(--light-blue)',
+        'pastel-blue': 'var(--pastel-blue)',
+        'strawberry-red': 'var(--strawberry-red)',
+        'cool-grey': 'var(--cool-grey)',
+        'light-grey': 'var(--light-grey)',
         'mangolia': 'var(--mangolia)',
         'alabaster': 'var(--alabaster)',
         'white': 'var(--white)',
