d20ee73e391eeb030d478aab2eb7b54b38142da3
codersuresh <sd807141@gmail.com>
2023-09-01 08:41:01 +0545

    refactor: put personal info form in own component

--- components/RightSection.tsx
+++ components/RightSection.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Personal from '@/components/forms/Personal'
 
 const RightSection = () => {
     return (
@@ -9,26 +10,7 @@ const RightSection = () => {
                 <p className="text-cool-gray mt-1">Please provide your name, email address and phone number.</p>
             </div>
 
-            <form action='#' className="mt-10 flex flex-col">
-
-                <div>
-                    <label htmlFor="name" className="block mb-1">Name</label>
-                    <input type="text" id="name" placeholder="eg: Stephen King" className="border rounded mb-4 p-2 w-full" />
-                </div>
-
-                <div>
-                    <label htmlFor="email" className="block mb-1">Email Address</label>
-                    <input type="email" id="email" placeholder="eg: stephenking@lorem.com" className="border rounded mb-4 p-2 w-full" />
-                </div>
-
-                <div>
-                    <label htmlFor="phone" className="block mb-1">Phone Number</label>
-                    <input type="tel" id="phone" placeholder="eg: +1 234 567 890" className="border rounded mb-4 p-2 w-full" />
-                </div>
-
-                <button type="submit" className="bg-marine-blue w-fit self-end text-white rounded-lg px-4 py-2 mt-10 font-semibold">Next Step</button>
-
-            </form>
+            <Personal />
 
         </div>
   )
--- components/forms/Personal.tsx
+++ components/forms/Personal.tsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Personal = () => {
   return (
-    <div>Personal</div>
+    <form action='#' className="mt-10 flex flex-col">
+
+      <div>
+        <label htmlFor="name" className="block mb-1">Name</label>
+        <input type="text" id="name" placeholder="eg: Stephen King" className="border rounded mb-4 p-2 w-full" />
+      </div>
+
+      <div>
+        <label htmlFor="email" className="block mb-1">Email Address</label>
+        <input type="email" id="email" placeholder="eg: stephenking@lorem.com" className="border rounded mb-4 p-2 w-full" />
+      </div>
+
+      <div>
+        <label htmlFor="phone" className="block mb-1">Phone Number</label>
+        <input type="tel" id="phone" placeholder="eg: +1 234 567 890" className="border rounded mb-4 p-2 w-full" />
+      </div>
+
+      <button type="submit" className="bg-marine-blue w-fit self-end text-white rounded-lg px-4 py-2 mt-10 font-semibold">Next Step</button>
+
+    </form>
   )
 }
 
