7604d809d9b695560dc480ec985cc8f2205c2955
codersuresh <sd807141@gmail.com>
2023-09-04 10:14:42 +0545

    fix: keep id of checkbox unique

--- components/forms/addons/AddonCard.tsx
+++ components/forms/addons/AddonCard.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { AddonProps } from '@/types/props'
 
 const AddonCard = ({ title, description, price }: AddonProps) => {
+
+    const id = title.toLowerCase().replace(' ', '-')
+
     return (
         <div className="border w-full rounded-lg p-5 flex items-center gap-3 mb-4">
-            <input type="checkbox" id="online-service" />
-            <label htmlFor="online-service" className='flex items-center w-full'>
+            <input type="checkbox" id={id} />
+            <label htmlFor={id} className='flex items-center w-full'>
                 <div className='flex-grow'>
                     <h3 className='font-semibold'>{title}</h3>
                     <p className="text-cool-gray">{description}</p>
